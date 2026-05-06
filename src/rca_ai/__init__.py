606f837f0d5c81a5ef3f00882c11c6bbe88607eb
alomarhw <alomarhw@miamioh.edu>
2026-05-06 02:06:07 -0400

    Add interactive web UI

---
+++ src/rca_ai/__init__.py
@@ -0,0 +1,6 @@
+"""RCA-AI platform implementation package."""
+
+from rca_ai.models import AIAssistRequest, Project, SuggestedPatch
+from rca_ai.platform import ResearchPaperPlatform
+
+__all__ = ["AIAssistRequest", "Project", "ResearchPaperPlatform", "SuggestedPatch"]
